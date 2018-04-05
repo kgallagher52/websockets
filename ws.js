a101a1e02ecc13e3a66e6cd610ef91e9567aaf36
Keaton Gallagher <kgallagher@CIT-SCC113-1125.cs.dixie.edu>
2018-04-05 11:30:12 -0600

    adding files

---
+++ ws.js
@@ -0,0 +1,29 @@
+var myName       = window.prompt("Hi. What's your name?");
+var messageInput = document.querySelector("#message-input");
+var messageLog   = document.querySelector("#message-log");
+var sendButton   = document.querySelector("#send-button");
+
+var logMessage = function (message) {
+  var el = document.createElement("div");
+  el.innerHTML = message;
+  messageLog.appendChild(el);
+};
+
+sendButton.onclick = function () {
+  var message = messageInput.value;
+  socket.send(myName + " Says: " + message);
+};
+
+// Making a websocket
+var socket = new WebSocket('ws://144.38.197.198:8080');
+
+// Making an event with a function it is known as event handler
+//  events also give you a event object function (event)
+socket.onopen = function () {
+    alert("connecte3d to server...");
+};
+
+socket.onmessage = function (event) {
+    console.log("Message Recived", event)
+    logMessage(event.data);
+}
---
+++ wss.js
@@ -0,0 +1,25 @@
+const WebSocket = require ('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+
+// Brodcast all to all clients whenever I want
+wss.brodcast = function brodcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+wss.on('connection', function(ws) {
+    ws.on('message', function (data) {
+        // This is checking the client and when they send a messeage
+        // brodcast the message
+        wss.clients.forEach(function (client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    })
+})
